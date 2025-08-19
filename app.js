b2a5e162d09d30e678a9704e4219d14766a211ba
Guinoob27 <azertylclavier27@hotmail.com>
2025-08-19 14:20:58 +0200

    Add MongoDB connection + book models

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
+const mongoose = require("mongoose");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.DB_URL}`
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json()); // Middleware permettant d'avoir acces au body de la requete
+
+app.post("/api/books", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Livre créé !",
+  });
 });
 
 app.use((req, res, next) => {
-  res.status(201);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+// app.get("/api/books", (req, res, next) => {
+//   Book.find()
+//     .then((books) => res.status(200).json(books))
+//     .catch((error) => res.status(400).json({ error }));
+// });
 
 module.exports = app;
